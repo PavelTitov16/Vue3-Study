2384fe8982b939cc4d75cef89fc70b94dc051eb7
PavelTitov16 <pashoktitov16@gmail.com>
2024-07-07 18:42:22 +0300

    feat: add modal window component with summon logic

--- posts-manager/src/App.vue
+++ posts-manager/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
-    <PostForm @create="createPost" />
+    <h1>Posts Manager</h1>
+    <my-button @click="showModal" class="modal-btn">Create Post</my-button>
+    <my-modal v-model:show="modalVisible">
+      <PostForm @create="createPost" />
+    </my-modal>
     <PostList :posts="posts" @remove="removePost" />
   </div>
 </template>
@@ -21,14 +25,19 @@ export default {
         { id: 2, title: 'Vue', body: 'Framework for JS' },
         { id: 3, title: 'React', body: 'Library for JS' },
       ],
+      modalVisible: false,
     };
   },
   methods: {
     createPost(post) {
       this.posts.push(post);
+      this.modalVisible = false;
     },
     removePost(post) {
       this.posts = this.posts.filter((currentPost) => currentPost.id !== post.id);
+    },
+    showModal() {
+      this.modalVisible = true;
     },
   },
 };
@@ -43,5 +52,9 @@ export default {
 
 .app {
   padding: 20px;
+}
+
+.modal-btn {
+  margin: 15px 0;
 }
 </style>
--- posts-manager/src/components/common/index.js
+++ posts-manager/src/components/common/index.js
@@ -1,7 +1,9 @@
 import MyButton from '@/components/common/MyButton.vue';
 import MyInput from '@/components/common/MyInput.vue';
+import MyModal from '@/components/common/MyModal.vue';
 
 export default [
   MyButton,
   MyInput,
+  MyModal,
 ];
