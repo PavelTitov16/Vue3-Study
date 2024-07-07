a28e4b4631ca54c1c52f869817bbe7622bf28c34
PavelTitov16 <pashoktitov16@gmail.com>
2024-07-07 14:00:26 +0300

    feat: add v-on directive

--- posts-manager/src/App.vue
+++ posts-manager/src/App.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>works</div>
+  <div>
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button v-on:click="addDislike">Dislike</button>
+    </div>
+    <div>
+      Likes
+      <strong>{{ likes }}</strong>
+    </div>
+    <div>
+      Dislikes
+      <strong>{{ dislikes }}</strong>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  data() {
+    return {
+      likes: 0,
+      dislikes: 0,
+    };
+  },
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    },
+  },
 };
 </script>
 
